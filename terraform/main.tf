1a81ad06dce0f0c3a903182e0830cea2879285a5
yoel <yp3@gmail.com>
2024-01-07 19:59:47 +0200

    version

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+ backend "s3" {
+    bucket         	   = "terra-test12"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+
+  }
+  required_version = ">= 1.2.0"
+}
+
+
+provider "aws" {
+  region  = "us-east-1"
+
+}
+
+
+resource "aws_instance" "example_server" {
+  ami = "ami-0fa07436ad11fff06"
+  instance_type = "t2.micro"
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo docker run yp3yp3/github:latest",
+    ]
+  }
+
+  tags = {
+    Name = "my-app"
+  }
+}
