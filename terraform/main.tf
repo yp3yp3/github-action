34f4a9ee569e9ec5b93587bbf4cb920b4837c2e6
yoel <yp3@gmail.com>
2024-01-07 20:23:54 +0200

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">=0.14.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       Version = "~>3.27"
     }
   }
-  required_version = ">=0.14.9"
+
  backend "s3" {
     bucket         	   = "terra-test12"
     key              	   = "state/terraform.tfstate"
